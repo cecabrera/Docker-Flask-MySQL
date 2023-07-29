1afaa859987894f240dbce12b3e652abbb57bdd9
Camilo Cabrera <cecabrera55@gmail.com>
2023-07-29 15:33:47 -0500

    Start up config

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+# Setup the server
+apt-get -y install build-essential python3-dev
+apt -y install docker.io
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Run the container
 docker-compose up -d
