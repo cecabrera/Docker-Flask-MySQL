32177a3750b6948a3a48602d36ef7ee53edd8cb0
Camilo Cabrera <cecabrera55@gmail.com>
2023-07-29 21:47:22 -0500

    src functions

---
+++ flask_file_upload/src/upload_csv.py
@@ -0,0 +1,30 @@
+from pandas import read_csv, read_sql
+
+
+def upload_csv(con, filepath, table_name):
+
+    # Read the CSV as a DataFrame
+    df = read_csv(
+        filepath_or_buffer=filepath,
+        header=None)
+
+    # Get Column Names
+    df.columns = read_sql(
+        con=con,
+        sql=f"SELECT * FROM {table_name} LIMIT 0"
+    ).columns
+
+    # Upload the CSV data into the table
+    df.to_sql(
+        con=con,
+        if_exists="append",
+        name=table_name,
+        index=False,
+        method="multi")
+
+    n = read_sql(
+        con=con,
+        sql=f"SELECT COUNT(*) AS n FROM {table_name}"
+    ).values[0][0]
+
+    print(f"{df.shape[0]} rows added into `{table_name}` table. Nrows: {n}")
