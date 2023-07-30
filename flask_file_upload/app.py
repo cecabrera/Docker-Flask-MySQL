18c981bc5c881ff3f87c4e1bf40cd48edc3628bd
Camilo Cabrera <cecabrera55@gmail.com>
2023-07-29 21:47:38 -0500

    include new csv files as tables

--- flask_file_upload/app.py
+++ flask_file_upload/app.py
@@ -1,17 +1,29 @@
 # Taken from: https://github.com/sathyainfotech/Excel-File-Upload-SQLite/tree/main
 
 from flask import Flask,render_template,request,flash,redirect,url_for
-import os
+from os.path import join
 import sqlite3
 import pandas as pd
+from src.readSQL import readSQL
+from src.upload_csv import upload_csv
 
 
 app=Flask(__name__)
 app.config['UPLOAD_FOLDER']="static\Excel"
 app.secret_key="123"
 
+sql_tables_path = [
+    "sql\\create\\jobs.sql",
+    "sql\\create\\departments.sql",
+    "sql\\create\\hired_employees.sql"
+]
+
 con=sqlite3.connect("MyData.db")
 con.execute("create table if not exists data(pid integer primary key,exceldata TEXT)")
+for sql_create_path in sql_tables_path:
+    sql = readSQL(filename=sql_create_path)
+    con.execute(sql)
+con.commit()
 con.close()
 
 @app.route("/",methods=['GET','POST'])
@@ -28,23 +40,42 @@ def index():
         uploadExcel = request.files['uploadExcel']
         print("Test aqui: ", uploadExcel.filename)
         if uploadExcel.filename != '':
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], uploadExcel.filename)
+
+            filepath = join(app.config['UPLOAD_FOLDER'], uploadExcel.filename)
             uploadExcel.save(filepath)
+
+            # Insert the file name into the `data.exceldata` column 
             con = sqlite3.connect("MyData.db")
             cur = con.cursor()
             cur.execute("insert into data(exceldata)values(?)", (uploadExcel.filename,))
             con.commit()
             flash("Excel Sheet Upload Successfully", "success")
 
+            # Create a table with the filename in case it does not exists
+            con = sqlite3.connect("MyData.db")
+            cur = con.cursor()
+            table_name = uploadExcel.filename.replace(".csv", "")
+
+            # Uploads a local file into a table
+            upload_csv(con=con, filepath=filepath, table_name=table_name)
+
+            # Loads data table content inside the table
             con = sqlite3.connect("MyData.db")
             con.row_factory = sqlite3.Row
             cur = con.cursor()
             cur.execute("select * from data")
             data = cur.fetchall()
             con.close()
-            return render_template("ExcelUpload.html", data=data)
 
-    return render_template("ExcelUpload.html",data=data)
+            template = render_template("ExcelUpload.html", data=data)
+
+            return template
+
+    template = render_template(
+        template_name_or_list="ExcelUpload.html",
+        data=data)
+
+    return template
 
 @app.route('/view_excel/<string:id>')
 def view_excel(id):
@@ -59,7 +90,15 @@ def view_excel(id):
         print(val[1])
         data=pd.read_csv(path)
     con.close()
-    return render_template("view_excel.html",data=data.to_html(index=False,classes="table table-bordered").replace('<th>','<th style="text-align:center">'))
+
+    template = render_template(
+        template_name_or_list="view_excel.html",
+        data=data.to_html(
+            index=False,
+            classes="table table-bordered"
+        ).replace('<th>','<th style="text-align:center">'))
+
+    return template
 
 @app.route('/delete_record/<string:id>')
 def delete_record(id):
